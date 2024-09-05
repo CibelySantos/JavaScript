52709f1cabfb86c36ae7a8e6a6dc3ef27ab42428
CibelySantos <168744739+CibelySantos@users.noreply.github.com>
2024-09-05 16:01:11 -0300

    Delete EFALEC directory

--- EFALEC/ex7 (cibely)/main.js
+++
@@ -1,7 +0,0 @@
-// Cibely Cristiny n°04
-// Gabriel Moreira n°06
-
-var frutas = ["Morango", "Banana", "Laranja", "Abacaxi", "Maçã"]; 
-    frutas.forEach(function(fruta){ 
-        console.log(fruta);
-})
