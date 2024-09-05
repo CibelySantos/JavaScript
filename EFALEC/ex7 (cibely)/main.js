d0aea0acfd7f6411c8e7cf3bf1aae3fffa24c950
CibelySantos <168744739+CibelySantos@users.noreply.github.com>
2024-09-05 15:58:47 -0300

    Add files via upload

---
+++ EFALEC/ex7 (cibely)/main.js
@@ -0,0 +1,7 @@
+// Cibely Cristiny n°04
+// Gabriel Moreira n°06
+
+var frutas = ["Morango", "Banana", "Laranja", "Abacaxi", "Maçã"]; 
+    frutas.forEach(function(fruta){ 
+        console.log(fruta);
+})
